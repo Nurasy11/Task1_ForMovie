a026d1ad90b97227fcc6372c1b2be032ed35f04c
Nurasyl <nurasylqazaqwws@gmail.com>
2023-11-04 16:07:15 +0600

    checking

--- script.js
+++ script.js
@@ -22,3 +22,19 @@
 Проверить, чтобы все работало без ошибок в консоли */
 'use strict'
 
+let numberOfFilms= +prompt('Сколько фильмов вы уже посмотрели?', '');
+let personalMovieDB={
+    count:`${numberOfFilms}`,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false 
+};
+
+const a=prompt('Один из последних просмотренных фильмов?', ''),
+b=prompt('На сколько оцените его?', ''),
+c=prompt('Один из последних просмотренных фильмов?', ''),
+d=prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[a]=b;
